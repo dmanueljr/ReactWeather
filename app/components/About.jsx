1fc0c31b7764bf89340e2a5894006fd457d40aab
Daniel Manuel <dmanueljr@gmail.com>
2017-03-12 20:06:40 -0700

    Adds styles to page titles using custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 var About = function(props) {
     return (
         <div>
-            <h1 className="text-center">About</h1>
+            <h1 className="text-center page-title">About</h1>
                 <blockquote>
                     "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
                     <cite>Somewhere online</cite>
